5cf35a123f44fdeec4e8b9260d15f4cedef1d10a
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2020-03-10 16:42:28 +0100

    Funkcne vymeny v mape
    Funguje kazda vymena prazdneho s cislom a konce mapy su osetrene

--- zadanie/main.py
+++ zadanie/main.py
@@ -1,15 +1,90 @@
-class Policko:
-    def __init__(self, hodnotaPolicka): #hodnota policka je cislo od 1 po (n*m) - 1, prazdne miesto na hracej ploche symbolizuje 0
-        self.pocetPohybov = 0
-        self.hodnotaPolicka = hodnotaPolicka
+import copy
+
+def vypisMapu(mapa):
+    for i in range(len(mapa)):
+        for j in range(len(mapa[i])):
+            print(mapa[i][j], end=" ")
+        print("")
 
 
-hraciaPlocha = []
+def zmenaMinusRiadok(obmena, mapaZaciatok, riadokNula, stlpecNula):
+    obmena[riadokNula][stlpecNula] = mapaZaciatok[riadokNula - 1][stlpecNula]
+    obmena[riadokNula - 1][stlpecNula] = 0
+    return obmena
 
-riadky = int(input("Zadajte pocet riadkov"))
-stlpce = int(input("Zadajte pocet stlpcov"))
 
-for i in range(riadky):
-    hraciaPlocha = []
-    for j in range(stlpce):
-        hraciaPlocha.append(Policko(int(input("Zadaj hodnotu policka"))))
+def zmenaPlusRiadok(obmena, mapaZaciatok, riadokNula, stlpecNula):
+    obmena[riadokNula][stlpecNula] = mapaZaciatok[riadokNula + 1][stlpecNula]
+    obmena[riadokNula + 1][stlpecNula] = 0
+    return obmena
+
+
+def zmenaMinusStlpec(obmena, mapaZaciatok, riadokNula, stlpecNula):
+    obmena[riadokNula][stlpecNula] = mapaZaciatok[riadokNula][stlpecNula - 1]
+    obmena[riadokNula][stlpecNula - 1] = 0
+    return obmena
+
+
+def zmenaPlusStlpec(obmena, mapaZaciatok, riadokNula, stlpecNula):
+    obmena[riadokNula][stlpecNula] = mapaZaciatok[riadokNula][stlpecNula + 1]
+    obmena[riadokNula][stlpecNula + 1] = 0
+    return obmena
+
+
+def skusaj(mapaZaciatok, mapaKoniec, riadokNula, stlpecNula):
+    if (riadokNula - 1 >= 0):
+        print("Riadok minus")
+        obmena = copy.deepcopy(mapaZaciatok)
+        zmenaMinusRiadok(obmena, mapaZaciatok, riadokNula, stlpecNula)
+        vypisMapu(obmena)
+    if (riadokNula + 1 < len(mapaZaciatok)):
+        print("Riadok plus")
+        obmena = copy.deepcopy(mapaZaciatok)
+        zmenaPlusRiadok(obmena, mapaZaciatok, riadokNula, stlpecNula)
+        vypisMapu(obmena)
+    if (stlpecNula - 1 >= 0):
+        print("Stlpec minus")
+        obmena = copy.deepcopy(mapaZaciatok)
+        zmenaMinusStlpec(obmena, mapaZaciatok, riadokNula, stlpecNula)
+        vypisMapu(obmena)
+    if (stlpecNula + 1 < len(mapaZaciatok[riadokNula])):
+        print("Stlpec plus")
+        obmena = copy.deepcopy(mapaZaciatok)
+        zmenaPlusStlpec(obmena, mapaZaciatok, riadokNula, stlpecNula)
+        vypisMapu(obmena)
+
+
+vygenerovaneMapy = []
+mapaKoniec = [[1, 2, 3],
+              [4, 6, 5],
+              [7, 8, 0]]
+mapaZaciatok = [[1, 2, 3],
+                [4, 8, 6],
+                [7, 5, 0]]
+
+stringMapy = ""
+riadokNula = stlpecNula = 0
+for i in range(len(mapaZaciatok)):
+    for j in range(len(mapaZaciatok[i])):
+        if (mapaZaciatok[i][j] is 0):
+            riadokNula = i
+            stlpecNula = j
+        print(mapaZaciatok[i][j], end=" ")
+        stringMapy = stringMapy + str(mapaZaciatok[i][j])
+    print("")
+
+
+
+pocetRoznych = 0
+for i in range(len(mapaZaciatok)):
+    for j in range(len(mapaZaciatok[i])):
+        if (mapaKoniec[i][j] is not mapaZaciatok[i][j]):
+            pocetRoznych = pocetRoznych + 1
+
+# print(str(riadokNula) + " " + str(stlpecNula))
+
+skusaj(mapaZaciatok, mapaKoniec, riadokNula, stlpecNula)
+print("")
+vypisMapu(mapaZaciatok)
+
+
